707c7fd5cd736da22e5f8a9b93ed421ec0e964f9
simon <trns0286@humber.ca>
2023-07-25 18:36:02 -0400

    obfuscate api key, output readable json, added random word gen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { generate, count } from "random-words";
+import API_KEY from "./api";
+import HOST from "./host";
 
 function App() {
+  const [data, setData] = useState(null);
+  const newWord = generate();
+  //json format may change,for example with word nor
+  const url = "https://wordsapiv1.p.rapidapi.com/words/" + newWord;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": HOST,
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url, options);
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? (
+        <div>
+          <pre>{JSON.stringify(data)}</pre>
+          <p>{data["word"]}</p>
+          <p>{data["results"][0]["definition"]}</p>
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
 }

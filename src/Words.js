e3f78dddf1b57336e70ffd39f21a8103a7c182ee
simon <trns0286@humber.ca>
2023-08-01 18:18:26 -0400

    bar file

---
+++ src/Words.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import generate from "random-words";
+
+const ApiComponent2 = () => {
+  const [data, setData] = useState(null);
+  const [newWord, setNewWord] = useState(generate()); // Track the current word
+
+  const url = "https://wordsapiv1.p.rapidapi.com/words/" + newWord;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "e7d3d20c19msh9909da35cc85505p1c98c1jsn6c2e7f6dc962",
+      "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+    },
+  };
+
+  const fetchWordData = async () => {
+    try {
+      const response = await axios.get(url, options);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWordData();
+  }, [newWord]); // Run the effect whenever newWord changes
+
+  const handleNewWord = () => {
+    const newWord = generate();
+    setNewWord(newWord); // Update state with the new word, triggering the effect
+  };
+
+  return (
+    <div>
+      <p>Word: {data && data.word}</p>
+      <p>Definition: </p>
+      <div>
+        {data &&
+          data.results &&
+          data.results.map((result, index) => (
+            <span key={index}>
+              {result.definition}
+              {index !== data.results.length - 1 && ", "}
+              <br />
+              <br />
+            </span>
+          ))}
+      </div>
+      <button onClick={handleNewWord}>New Word</button>
+    </div>
+  );
+};
+
+export default ApiComponent2;

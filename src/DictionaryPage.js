9244afdda71e9c4ba50b5936c3b6cece6b19cc02
Mark <n01586869@humber.ca>
2023-08-03 14:56:00 -0400

    added DictionaryPage

---
+++ src/DictionaryPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const DictionaryPage = () => {
+  return (
+    <Container className="mt-5">
+      <h1>Dictionary</h1>
+      {/* Add the search bar here */}
+      {/* Replace this with your own implementation */}
+    </Container>
+  );
+};
+
+export default DictionaryPage;

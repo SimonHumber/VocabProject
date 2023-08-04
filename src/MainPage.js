feae795b01056e6707d5b85fca15a1218b026ba0
Mark <n01586869@humber.ca>
2023-08-04 00:30:58 -0400

    added MainPage component

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,31 +1,51 @@
 import React, { useState } from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Row, Col, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './mainPage.css'
 
 const MainPage = () => {
   const [word, setWord] = useState('');
 
   const generateNewWord = () => {
-    // Replace this with your own logic to generate a new word
-    const newWord = 'ExampleWord';
-    setWord(newWord);
   };
 
   const saveWord = () => {
-    // Replace this with your own logic to save the word
-    // You can save it to an array and display the list on the right-hand side
   };
 
+  // return (
+  //   <Container className="mt-5">
+  //     <h1>Word: {word}</h1>
+  //     <Button variant="primary" onClick={generateNewWord}>
+  //       Generate
+  //     </Button>
+  //     <Button variant="success" className="ml-3" onClick={saveWord}>
+  //       Save
+  //     </Button>
+  //     {/* Add the word list on the right-hand side */}
+  //     {/* Replace this with your own implementation */}
+  //   </Container>
+  // );
   return (
-    <Container className="mt-5">
-      <h1>Word: {word}</h1>
-      <Button variant="primary" onClick={generateNewWord}>
-        Generate
-      </Button>
-      <Button variant="success" className="ml-3" onClick={saveWord}>
-        Save
-      </Button>
-      {/* Add the word list on the right-hand side */}
-      {/* Replace this with your own implementation */}
+    <Container fluid className="main-page-container bg-dark text-light">
+      <Row className="justify-content-center align-items-center main-page-row">
+        <Col className="text-center">
+        <Button variant="primary" className="generate-btn">Generate</Button>
+          <h1 className="word">Word</h1>
+          <p className="definition">Definition of the Word</p>
+          <Button variant="primary" className="add-btn">Add</Button>
+        </Col>
+        <Col>
+          <div className="sidebar">
+            <Form className="search-bar mb-3">
+              <Form.Control type="text" placeholder="Search..." />
+            </Form>
+            <ul className="word-list">
+              <li>Word1</li>
+              <li>Word2</li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };

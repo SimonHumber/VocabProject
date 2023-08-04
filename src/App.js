2495261d7d355a6171332cbecf18f896c145d752
Mark <n01586869@humber.ca>
2023-08-03 23:54:28 -0400

    Added all 3 components to App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WelcomePage from './WelcomePage';
+import MainPage from './MainPage';
+import DictionaryPage from './DictionaryPage';
+
+const App = () => {
+  return (
+    <WelcomePage />
+    // <MainPage />
+    // <DictionaryPage />
+  );
+};
+
+export default App;

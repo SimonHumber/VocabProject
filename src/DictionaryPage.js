3d562b164ec972e5ea7fe304d442a5b29d8054ff
Mark <n01586869@humber.ca>
2023-08-05 03:40:37 -0400

    create form + button + encase in Row and Col

--- src/DictionaryPage.js
+++ src/DictionaryPage.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import NavBar from './NavBar';
 import './dictionaryPage.css'
 
 const DictionaryPage = () => {
   return (
-    <Container fluid className="dictionary-page-container mt-5 bg-dark text-light">
+    <Container fluid className="dictionary-page-container bg-dark text-light">
       <NavBar />
-      <h1>Dictionary</h1>
-      {/* Add the search bar here */}
-      {/* Replace this with your own implementation */}
+      <Row className="align-items-center">
+        <Col>
+          <h1 className="dictionary">Dictionary</h1>
+          <Form className="mb-3">
+            <Form.Control className="dictionary-search-bar"type="text" placeholder="Search..." />
+          </Form>
+          <Button variant="primary" className="search-btn">Search</Button>
+        </Col>
+      </Row>
     </Container>
   );
 };

fe2bc2ea4c243f60c65598dadae7f3e21d92eacf
Mark <n01586869@humber.ca>
2023-08-05 03:17:28 -0400

    imported and added navbar to page

--- src/DictionaryPage.js
+++ src/DictionaryPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import NavBar from './NavBar';
 
 const DictionaryPage = () => {
   return (
     <Container className="mt-5">
+      <NavBar />
       <h1>Dictionary</h1>
       {/* Add the search bar here */}
       {/* Replace this with your own implementation */}

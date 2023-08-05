7b5dd30c014b90df1d3baa7f8694ed35f78cdfb0
Mark <n01586869@humber.ca>
2023-08-05 03:21:04 -0400

    changed container classes

--- src/DictionaryPage.js
+++ src/DictionaryPage.js
@@ -4,7 +4,7 @@ import NavBar from './NavBar';
 
 const DictionaryPage = () => {
   return (
-    <Container className="mt-5">
+    <Container fluid className="dictionary-page-container mt-5 bg-dark text-light">
       <NavBar />
       <h1>Dictionary</h1>
       {/* Add the search bar here */}

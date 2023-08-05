250a71a23cd43b16f36fe0cb12751cf6b544f569
Mark <n01586869@humber.ca>
2023-08-05 03:14:41 -0400

    changed imports,comments,useEffect,navBar, classes

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,29 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Button, Row, Col, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './mainPage.css'
+import NavBar from './NavBar';
 
 const MainPage = () => {
+
   const [word, setWord] = useState('');
 
   const generateNewWord = () => {
+    // generate word code
   };
 
   const saveWord = () => {
+    // save word code
   };
+
+  useEffect(()=>{
+    // API call
+  }, [])
 
   return (
     <Container fluid className="main-page-container bg-dark text-light">
+      <NavBar />
       <Row className="justify-content-center align-items-center main-page-row">
         <Col className="text-center">
-        <Button variant="primary" className="generate-btn">Generate</Button>
+        <Button variant="primary" className="generate-btn" onClick={generateNewWord}>Generate</Button>
           <h1 className="word">Word</h1>
           <p className="definition">Definition of the Word</p>
-          <Button variant="primary" className="add-btn">Add</Button>
+          <Button variant="primary" className="add-btn" onClick={saveWord}>Add</Button>
         </Col>
         <Col>
           <div className="sidebar">
-            <Form className="search-bar mb-3">
+            <Form className="mb-3">
               <Form.Control type="text" placeholder="Search..." />
             </Form>
             <ul className="word-list">

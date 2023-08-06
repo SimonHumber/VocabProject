12c934fe0a7556b2f853e03a6c7d76cfd75f6810
Mark <n01586869@humber.ca>
2023-08-06 14:22:22 -0400

    made button link to main page

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -3,16 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './welcomePage.css';
+import { Link } from "react-router-dom";
 
 const WelcomePage = () => {
 
   const handleGoToMainPage = () => {
+
   };
 
   return (
     <Container fluid className="welcome-page-container bg-dark">
       <h1 className="welcome-title text-primary">Welcome to the word generation app</h1>
+      <Link to="./main">
       <Button variant="primary" onClick={handleGoToMainPage}>Start your journey!</Button>
+      </Link>
     </Container>
   );
 };

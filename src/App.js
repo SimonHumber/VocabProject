e7d6a1220e7c0ff0b4c403bad9455a9c10d17804
Mark <n01586869@humber.ca>
2023-08-06 14:19:52 -0400

    added routing to App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import WelcomePage from './WelcomePage';
 import MainPage from './MainPage';
 import DictionaryPage from './DictionaryPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      Welcome Page
-    <WelcomePage />
-    Main Page
-    <MainPage />
-    Dictionary Page
-    <DictionaryPage />
+    // <div>
+    //   Welcome Page
+    // <WelcomePage />
+    // Main Page
+    // <MainPage />
+    // Dictionary Page
+    // <DictionaryPage />
+    // </div>
+    <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<WelcomePage />}/>
+        <Route path="/mainApp" element={<MainPage />}/>
+        <Route path="/dictionary" element={<DictionaryPage />} />
+      </Routes>
     </div>
+  </BrowserRouter>
   );
 };
 

9519b6a89e204be15115a183322f478d141e64aa
Mark <n01586869@humber.ca>
2023-08-06 15:34:41 -0400

    Made navbar functional

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap/';
+import { Nav, Button } from 'react-bootstrap/';
+import { Link } from 'react-router-dom';
+import './navBar.css';
 
-const NavBar = () => {
+const NavBar = (props) => {
+
   return (
-    <Nav className="nav-bar" variant="pills" defaultActiveKey="/home">
+      <Nav 
+      className="nav-bar" 
+      variant="pills" 
+      defaultActiveKey="/main" 
+      activeKey={props.page}>
       <Nav.Item>
-        <Nav.Link href="/main">Generate</Nav.Link>
+        <Button variant="primary" size="sm" className="nav-button">
+          <Link to="/main" className="btn">Generate</Link>
+        </Button>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/dictionary">Dictionary</Nav.Link>
+        <Button variant="primary" size="sm" className="nav-button">
+          <Link to="/dictionary" className="btn">Dictionary</Link>
+        </Button>
       </Nav.Item>
-    </Nav>
+      </Nav>
   );
 };
 

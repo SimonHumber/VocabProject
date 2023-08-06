57f1f9e6772be85a0237b0adeffae1c792770791
Mark <n01586869@humber.ca>
2023-08-06 14:51:41 -0400

    made nav buttons functional

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
   return (
     <Nav className="nav-bar" variant="pills" defaultActiveKey="/home">
       <Nav.Item>
-        <Nav.Link href="/home">Generate</Nav.Link>
+        <Nav.Link href="/main">Generate</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="dictionary">Dictionary</Nav.Link>
+        <Nav.Link href="/dictionary">Dictionary</Nav.Link>
       </Nav.Item>
     </Nav>
   );
